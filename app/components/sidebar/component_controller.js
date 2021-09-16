87f85edb7c91761512e28fbb68d61c01d23addaf
Dieter Lunn <coder2000@gmail.com>
2021-09-16 16:57:20 -0500

    feat: make sidebar work on mobile

--- app/components/sidebar/component_controller.js
+++ app/components/sidebar/component_controller.js
@@ -1,7 +1,25 @@
-import { Controller } from "stimulus";
-
+import { Controller } from "@hotwired/stimulus";
+import { enter, leave } from "el-transition";
 export default class extends Controller {
-  connect() {
-    console.log("Hello, Stimulus!", this.element);
+  static targets = ["overlay", "menu", "button", "modal"];
+
+  show(event) {
+    event.preventDefault();
+
+    this.modalTarget.classList.remove("hidden");
+
+    enter(this.overlayTarget, "sidebar-overlay");
+    enter(this.menuTarget, "sidebar-menu");
+    enter(this.buttonTarget, "sidebar-button");
+  }
+
+  hide(event) {
+    event.preventDefault();
+
+    this.modalTarget.classList.add("hidden");
+
+    leave(this.overlayTarget, "sidebar-overlay");
+    leave(this.menuTarget, "sidebar-menu");
+    leave(this.buttonTarget, "sidebar-button");
   }
 }
