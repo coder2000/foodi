5084185da52de643669245728d69e6c1e56864d9
Dieter Lunn <coder2000@gmail.com>
2021-09-16 20:46:44 -0500

    feat: add search bar

--- app/helpers/icon_helper.rb
+++ app/helpers/icon_helper.rb
@@ -1,6 +1,6 @@
 module IconHelper
   def fa_icon(name, options = {})
-    weight = options.delete(:weight) || "fat"
+    weight = options.delete(:weight) || "fa-thin"
     options[:class] = [weight, "fa-#{name}", options.delete(:class)]
     tag.i(nil, **options)
   end
