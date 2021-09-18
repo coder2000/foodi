bcfbadee3a8ecc4990b5765f428611754799691e
Dieter Lunn <coder2000@gmail.com>
2021-09-18 15:24:22 -0500

    feat: add static index page

--- config/routes.rb
+++ config/routes.rb
@@ -33,6 +33,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :supplies
 
-  root to: "supplies#index"
+  root to: "static#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
