6d3a3b18476e3fcdeb93d390c30ebd0526c40d2d
Dieter Lunn <coder2000@gmail.com>
2021-09-24 15:22:46 -0500

    feat: add sign in form

--- config/routes.rb
+++ config/routes.rb
@@ -1,36 +1,42 @@
 # == Route Map
 #
-#                            Prefix Verb   URI Pattern                            Controller#Action
-#                  new_user_session GET    /users/sign_in(.:format)               devise/sessions#new
-#                      user_session POST   /users/sign_in(.:format)               devise/sessions#create
-#              destroy_user_session DELETE /users/sign_out(.:format)              devise/sessions#destroy
-#                 new_user_password GET    /users/password/new(.:format)          devise/passwords#new
-#                edit_user_password GET    /users/password/edit(.:format)         devise/passwords#edit
-#                     user_password PATCH  /users/password(.:format)              devise/passwords#update
-#                                   PUT    /users/password(.:format)              devise/passwords#update
-#                                   POST   /users/password(.:format)              devise/passwords#create
-#          cancel_user_registration GET    /users/cancel(.:format)                devise/registrations#cancel
-#             new_user_registration GET    /users/sign_up(.:format)               devise/registrations#new
-#            edit_user_registration GET    /users/edit(.:format)                  devise/registrations#edit
-#                 user_registration PATCH  /users(.:format)                       devise/registrations#update
-#                                   PUT    /users(.:format)                       devise/registrations#update
-#                                   DELETE /users(.:format)                       devise/registrations#destroy
-#                                   POST   /users(.:format)                       devise/registrations#create
-#                          supplies GET    /supplies(.:format)                    supplies#index
-#                                   POST   /supplies(.:format)                    supplies#create
-#                        new_supply GET    /supplies/new(.:format)                supplies#new
-#                       edit_supply GET    /supplies/:id/edit(.:format)           supplies#edit
-#                            supply GET    /supplies/:id(.:format)                supplies#show
-#                                   PATCH  /supplies/:id(.:format)                supplies#update
-#                                   PUT    /supplies/:id(.:format)                supplies#update
-#                                   DELETE /supplies/:id(.:format)                supplies#destroy
-#                              root GET    /                                      supplies#index
-#  turbo_recede_historical_location GET    /recede_historical_location(.:format)  turbo/native/navigation#recede
-#  turbo_resume_historical_location GET    /resume_historical_location(.:format)  turbo/native/navigation#resume
-# turbo_refresh_historical_location GET    /refresh_historical_location(.:format) turbo/native/navigation#refresh
+#                                Prefix Verb     URI Pattern                                  Controller#Action
+#                      new_user_session GET      /users/sign_in(.:format)                     users/sessions#new
+#                          user_session POST     /users/sign_in(.:format)                     users/sessions#create
+#                  destroy_user_session DELETE   /users/sign_out(.:format)                    users/sessions#destroy
+# user_google_oauth2_omniauth_authorize GET|POST /users/auth/google_oauth2(.:format)          devise/omniauth_callbacks#passthru
+#  user_google_oauth2_omniauth_callback GET|POST /users/auth/google_oauth2/callback(.:format) devise/omniauth_callbacks#google_oauth2
+#      user_facebook_omniauth_authorize GET|POST /users/auth/facebook(.:format)               devise/omniauth_callbacks#passthru
+#       user_facebook_omniauth_callback GET|POST /users/auth/facebook/callback(.:format)      devise/omniauth_callbacks#facebook
+#                     new_user_password GET      /users/password/new(.:format)                devise/passwords#new
+#                    edit_user_password GET      /users/password/edit(.:format)               devise/passwords#edit
+#                         user_password PATCH    /users/password(.:format)                    devise/passwords#update
+#                                       PUT      /users/password(.:format)                    devise/passwords#update
+#                                       POST     /users/password(.:format)                    devise/passwords#create
+#              cancel_user_registration GET      /users/cancel(.:format)                      devise/registrations#cancel
+#                 new_user_registration GET      /users/sign_up(.:format)                     devise/registrations#new
+#                edit_user_registration GET      /users/edit(.:format)                        devise/registrations#edit
+#                     user_registration PATCH    /users(.:format)                             devise/registrations#update
+#                                       PUT      /users(.:format)                             devise/registrations#update
+#                                       DELETE   /users(.:format)                             devise/registrations#destroy
+#                                       POST     /users(.:format)                             devise/registrations#create
+#                              supplies GET      /supplies(.:format)                          supplies#index
+#                                       POST     /supplies(.:format)                          supplies#create
+#                            new_supply GET      /supplies/new(.:format)                      supplies#new
+#                           edit_supply GET      /supplies/:id/edit(.:format)                 supplies#edit
+#                                supply GET      /supplies/:id(.:format)                      supplies#show
+#                                       PATCH    /supplies/:id(.:format)                      supplies#update
+#                                       PUT      /supplies/:id(.:format)                      supplies#update
+#                                       DELETE   /supplies/:id(.:format)                      supplies#destroy
+#                                  root GET      /                                            static#index
+#      turbo_recede_historical_location GET      /recede_historical_location(.:format)        turbo/native/navigation#recede
+#      turbo_resume_historical_location GET      /resume_historical_location(.:format)        turbo/native/navigation#resume
+#     turbo_refresh_historical_location GET      /refresh_historical_location(.:format)       turbo/native/navigation#refresh
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: "users/sessions"
+  }
   resources :supplies
 
   root to: "static#index"
