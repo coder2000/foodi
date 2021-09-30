4edd3197bc6ded7db8b097143db6b84c2baedb00
Dieter Lunn <coder2000@gmail.com>
2021-09-30 11:53:04 -0500

    fix: move settings to before_render

--- app/components/sidebar/menu.rb
+++ app/components/sidebar/menu.rb
@@ -5,12 +5,6 @@ class Sidebar::Menu < ViewComponent::Base
     @classes = {link: "group flex items-center px-2 py-2 text-sm font-medium rounded-md", icon: "mr-4 flex-shrink-0 h-6 w-6 fa-xl fa-fw"}
     @active = {link: "bg-gray-900 text-white", icon: "text-gray-300"}
     @inactive = {link: "text-gray-300 hover:bg-gray-700 hover:text-white", icon: "text-gray-400 group-hover:text-gray-300"}
-
-    @settings = {
-      name: "Settings",
-      url: "#",
-      icon: "gear"
-    }
   end
 
   def before_render
@@ -46,6 +40,12 @@ class Sidebar::Menu < ViewComponent::Base
         icon: "calendar"
       }
     ]
+
+    @settings = {
+      name: "Settings",
+      url: "#",
+      icon: "gear"
+    }
 
     @logout = {
       name: "Log out",
